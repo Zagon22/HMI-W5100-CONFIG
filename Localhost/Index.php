9a0c5b216c4e1010a2f88397dd4c3ab2932f20f3
Gonzalo <94965367+Zagon22@users.noreply.github.com>
2022-11-12 16:57:17 -0300

    Update Index.php

--- Localhost/Index.php
+++ Localhost/Index.php
@@ -1,37 +1,42 @@
 <?PHP
-error_reporting(E_ALL ^ E_WARNING);
-$ledstat = $_GET['ledstat']; // Asignación del valor enviado por la petición GET a la variable "$ledstat".
-$enlace = mysqli_connect('127.0.0.1', 'root', '', 'conexion') // Establecimiento de la conexión con la base de datos.
-or die('No se puede conectar: '. mysqli_connect_error()); // Si el intento de conexión fallara, brindaría esta información.
-/* Si llegara a conectarse, enviaría la siguiente información. */
-echo 'Conexión Exitosa. <BR />';
-echo '<HR />';
-/* El valor alojado en "ledstat" es registrado en una entrada de la base de datos. */
-$consulta = mysqli_query($enlace, "INSERT INTO ledstatus(ledstat) VALUES ('$ledstat')");
-/* Si no pudiera efectuarse lo anterior, debería aparecer: */
-if(!$consulta) {
-echo "Error al guardar. <BR />";
-}
-/* En su defecto; o sea, si el valor de "ledstat" se hubiera registrado, aparecería: */
-else {
-echo "Datos Guardados. <BR />";
-}
-/* Impresión por pantalla de los datos (ordenados en una tabla). */
-echo "<HR />";
-echo "Registro de la base de datos: <BR />";
-echo "<HR />";
-echo "<TABLE BORDER=1 id='tabla'> 
-	<TR>
-	<TD> ID </TD>
-	<TD> Voltaje </TD>
-	</TR>";
-$sql = "SELECT * FROM ledstatus";
-$datos = mysqli_query($enlace, $sql);
-while($campo = mysqli_fetch_array($datos)) 
-{
-echo "<TR>
-	<TD>".$campo['id']."</TD>
-	<TD>".$campo['ledstat']."</TD>
-	</TR>"	;
-}
+	$ledstat = $_GET['ledstat']; // Obtención del valor aportado por la placa Arduino (cliente Web);
+	$enlace = mysqli_connect('127.0.0.1', 'root', '', 'conexion') // Establecimiento de la conexión con la base de datos.
+	or die('No se puede conectar: '. mysqli_connect_error());  // Información de error ante un fallo en el intento de conexión.
+	echo 'Conexión Exitosa. <BR />'; // Mensaje impreso al establecerse la conexión.
+	echo '<HR />'; // Inserción de un separador.
+
+	$consulta = mysqli_query($enlace, "INSERT INTO ledstatus(ledstat) VALUES ('$ledstat')"); // Inserción de los datos ingresados desde el cliente en la tabla indicada de la base de datos
+
+	/* Bifurcación del flujo del programa según la tarea anterior */
+	if (!$consulta) 
+	{
+		echo "Error al guardar. <BR />"; // Mensaje impreso en caso de no efectuarse la instrucción sql "INSERT".
+	}
+	else 
+	{
+		echo "Datos Guardados. <BR />"; // Mensaje impreso en caso de haberse efectuado la instrucción sql "INSERT".
+	}
+
+	echo "<HR />"; // Inserción de un separador.
+	echo "Registro de la base de datos: <BR />";
+	echo "<HR />"; // Inserción de un separador.
+
+	/* Confección de una tabla */
+	echo "<TABLE BORDER=1 id='tabla'> 
+		<TR>
+		<TD> ID </TD>
+		<TD> Voltaje </TD>
+		</TR>";
+
+	$sql = "SELECT * FROM ledstatus"; // Selección de los datos de la tabla indicada.
+	$datos = mysqli_query($enlace, $sql); // Ejecución de la selección previamente contemplada.
+
+	/* Impresión de los valores de la base de datos en la tabla */
+	while ($campo = mysqli_fetch_array($datos))
+	{
+		echo "<TR>
+			<TD>".$campo['id']."</TD>
+			<TD>".$campo['ledstat']."</TD>
+		       </TR>";
+	}
 ?>
